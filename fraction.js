127fa2692f8eb06e0edc17e0c49a6e7ba4e3efe1
Alisha Taj <114483067+AlishaTaj@users.noreply.github.com>
2023-04-20 12:02:55 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,18 @@
-class fraction
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+ subtract(fraction) {                                                                                                                                                                                                                                                                     
+    let newNumerator = (this.numerator * fraction.denominator) - (fraction.numerator * this.denominator);
+    let newDenominator = this.denominator * fraction.denominator;
+    return new Fraction(newNumerator, newDenominator);
+  }
+}
+
+const fraction1 = new Fraction(3, 4);
+const fraction2 = new Fraction(1, 4);
+
+const result = fraction1.subtract(fraction2);
+
+console.log(result);
