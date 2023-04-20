207c05f80c892cbec8a41b803c13e4e9a5b30666
phalgunisp <114557138+phalgunisp@users.noreply.github.com>
2023-04-20 12:14:25 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,11 @@
+ //divide two fractions using classes
+
+const prompt = require('prompt-sync')();
+
+class Fractions{
+  constructor(n1,d1,n2,d2){
+    this.num1 = n1*d2;
+    this.num2 = n2*d1;
+  }
+}
 
